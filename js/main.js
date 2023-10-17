e7b084eca915927a9048d489d1cfa80d6e8595f7
kerdaino <kerdaino3@gmail.com>
2023-10-17 07:50:35 -0700

    second-commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-const navL = document.querySelector('.navbar')
-const logo = document.querySelector('.nav-change')
+document.addEventListener('DOMContentLoaded', function() {
+  const navL = document.querySelector('.navbar');
+  const logo1 = document.getElementById('logo1');
+  const logo2 = document.getElementById('logo2');
+  const navbarNav = document.querySelector('.navbar');
 
-window.addEventListener('scroll' , () => {
-    if(window.scrollY >= 90) {
-        navL.classList.add('navbar-scrolled');
-        navL.classList.remove('nav-change');
-    } else if(window.scrollY < 80) {
-        navL.classList.remove('navbar-scrolled');
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= 90) {
+      navL.classList.add('navbar-scrolled');
+      logo1.style.display = 'none';
+      logo2.style.display = 'block';
+      navbarNav.classList.add('navbar-color-change');
+    } else if (window.scrollY < 90) {
+      navL.classList.remove('navbar-scrolled');
+      logo1.style.display = 'block';
+      logo2.style.display = 'none';
+      navbarNav.classList.remove('navbar-color-change');
     }
+  });
 });
